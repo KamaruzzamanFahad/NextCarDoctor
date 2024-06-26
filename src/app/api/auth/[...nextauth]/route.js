1480ff47f37daffce5dba703e5969b53af5ced7d
KamaruzzamanFahad <mihacker41@gmail.com>
2024-06-26 19:27:43 +0600

    next auth email pass sign in done

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,11 +1,24 @@
 import NextAuth from "next-auth/next";
-
+import CredentialsProvider from "next-auth/providers/credentials";
 const handler = NextAuth({
     session: {
         strategy: "jwt"
     },
     providers: [
-
+        CredentialsProvider({
+            credentials: {
+                email: { label: 'email', type: "text", placeholder: "enter email", required: true },
+                password: { label: 'password', type: "password", placeholder: "enter password", required: true }
+            },
+            async authorize(credentials) {
+                if (!credentials) {
+                    return null;
+                }
+                else {
+                    return true;
+                }
+            }
+        })
     ]
 })
 
